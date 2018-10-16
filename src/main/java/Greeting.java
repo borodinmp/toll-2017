405613ab71e2c9b868a8d5d0d6ca476aadd21119
Borodinmp <borodinmp@mail.ru>
2018-10-16 20:11:38 +0700

    Add groovy

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -2,6 +2,7 @@ public class Greeting {
 
     public static void main(String... args) {
         System.out.println("Hello! It is TUSUR student!");
+        System.out.println("JAVA PRO course");
     }
 
 }
